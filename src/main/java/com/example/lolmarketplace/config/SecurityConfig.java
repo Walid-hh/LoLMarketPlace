3f7c2908f7c5eb157d4b1c62fd3c3a8b68520a43
MidSleepin <walidelgoudali@gmail.com>
2024-05-12 19:49:08 +0100

    v6(Login Error Handlind)

--- src/main/java/com/example/lolmarketplace/config/SecurityConfig.java
+++ src/main/java/com/example/lolmarketplace/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.example.lolmarketplace.config;
 
 import com.example.lolmarketplace.services.UserLogin;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+import java.net.URLEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -18,6 +27,7 @@ public class SecurityConfig {
     public SecurityConfig(UserLogin userLogin) {
         this.userLogin = userLogin;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -27,23 +37,53 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf.disable())
+                .sessionManagement((session) -> session
+                        .invalidSessionUrl("/login")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                        .expiredUrl("/login?expired")
+                        .and())
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/logout")
+                                .invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                                .logoutSuccessUrl("/login")
+                )
+
                 .authorizeRequests((requests) -> requests
                         .requestMatchers(
                                 "/AddAccount", "/UpdateAccount"
                         ).authenticated()
                         .requestMatchers(
-                                "/index", "/", "/webjars/**" ,"/h2-console/**"
+                                "/index", "/", "/webjars/**", "/h2-console/**"
                         ).permitAll()
                 )
                 .userDetailsService(userLogin)
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/", true)
-                        .permitAll())
-                .headers().frameOptions().disable();
-               ;
-
+                        .permitAll()
+                        .failureHandler(customAuthenticationFailureHandler()))
+                .headers().frameOptions().disable()
+        ;
 
         return http.build();
+    }
+
+
+
+    private AuthenticationFailureHandler customAuthenticationFailureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException, IOException {
+                // Extract the error message from the exception
+                String errorMessage = exception.getMessage();
+
+                // Redirect the user back to the login page with the error message as a parameter
+                String encodedMessage = URLEncoder.encode(errorMessage, "UTF-8");
+                getRedirectStrategy().sendRedirect(request, response, "/login?error=" + encodedMessage);
+            }
+        };
     }
 }
--- src/main/java/com/example/lolmarketplace/controllers/IndexController.java
+++ src/main/java/com/example/lolmarketplace/controllers/IndexController.java
@@ -1,13 +1,29 @@
 package com.example.lolmarketplace.controllers;
 
+import com.example.lolmarketplace.dao.entities.User;
+import com.example.lolmarketplace.dao.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private final UserRepository userRepository;
+
+    public IndexController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
-    public String index() {
-        return "index"; // This should be the name of your index view file without the extension
+    public String index(Authentication authentication, HttpSession session) {
+        String username = authentication.getName();
+        User user = userRepository.findByUsername(username).get();
+        if (user != null) {
+            String name = user.getUsername();
+            session.setAttribute("username", name);
+        }
+        session.setAttribute("username", username);
+        return "index";
     }
 }
